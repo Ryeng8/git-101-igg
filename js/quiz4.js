4e0ba468260e911e225280cb770b1d9599fa612d
Ryeng8 <37680261+Ryeng8@users.noreply.github.com>
2020-09-16 15:47:01 +0700

    Revert "Revert "syncAndasync file""
    This reverts commit 73c08131b65d6022c10bee589d43a8d04b43ee1d.

---
+++ js/quiz4.js
@@ -0,0 +1,16 @@
+var str = "Hello my name is Joke"
+
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var stn = txt.length;
+
+index0f()
+var str = "Please locate where 'locate' occurs!";
+var pos = str.index0f("locate");
+
+search()
+var str = "Please locate where 'locate' occurs!";
+var pos = str.search("locate");
+
+slice()
+var str = "Apple, Banana, Kiwi";
+var res = str.slice(7, 13);
