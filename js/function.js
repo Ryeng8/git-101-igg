7875fbe01ad754a4e9094e5890d7e18ca1d9586c
Ryeng8 <37680261+Ryeng8@users.noreply.github.com>
2020-09-16 14:06:09 +0700

    added function file ,quiz1 file

---
+++ js/function.js
@@ -0,0 +1,18 @@
+// function add(a, b) {
+//     return a + b
+// }
+
+const add = (a, b) => a + b
+
+
+function mul(a, b) {
+    return a * b
+}
+
+function searchCustomer(customerName) {
+    // find some customer
+}
+
+console.log(add(20, 3))
+
+console.log(mul(20, 3))
---
+++ js/quiz1.js
@@ -0,0 +1,8 @@
+let person = {
+    firstName: "Ryeng",
+    lastName: "Vang",
+    age: "23",
+    habbies: "play game",
+    phone: "0882713963",
+}
+console.log(person);
